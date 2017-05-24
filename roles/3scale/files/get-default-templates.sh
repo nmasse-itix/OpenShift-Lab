b962cceabaf805938f911554b400c8f929030275
Nicolas Massé <nicolas.masse@itix.fr>
2017-05-24 17:58:13 +0200

    Work in progress

--- roles/3scale/files/get-default-templates.sh
+++ roles/3scale/files/get-default-templates.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #
-# This script gets the original 3scale template, converts it to JSON and
-# scale down the DeploymentConfig's replicas to 0.
+# This script gets the original 3scale template, converts it to JSON,
+# scales down the DeploymentConfig's replicas to 0 and disable triggers.
 #
 
 curl -s -o amp.yml https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.0.0.GA/amp/amp.yml
-yaml2json amp.yml |jq '(.objects[]|select(.kind== "DeploymentConfig").spec.replicas) |= 0' > amp.json
+yaml2json amp.yml |jq '(.objects[]|select(.kind== "DeploymentConfig").spec.replicas) |= 0 | (.objects[]|select(.kind== "DeploymentConfig").spec.triggers) |= []' > amp.json
